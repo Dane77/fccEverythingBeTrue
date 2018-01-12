a22090b0290c122b6d79812591e981a687ec8fff
Dane77 <bolesd77@gmail.com>
2018-01-12 17:21:14 -0500

    Create fccEverythingBeTrue.js

---
+++ fccEverythingBeTrue.js
@@ -0,0 +1,10 @@
+function truthCheck(collection, pre) {
+  
+  var counter = 0;
+  for (var c in collection){
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])){
+      counter++;
+    }
+  }
+  return counter == collection.length;
+}
